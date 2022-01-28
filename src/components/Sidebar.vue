6e5699be8c77f98d94797c9db466639d187c8463
dhruvi-wohlig <97166039+dhruvi-wohlig@users.noreply.github.com>
2022-01-28 17:24:07 +0530

    Add files via upload

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-navigation-drawer v-model="drawer" app>
+    <v-img
+      height="140"
+      class="pa-4"
+      src="https://images.unsplash.com/photo-1562657843-7b9e8c5ebb02?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=877&q=80"
+    >
+      <div class="text-center">
+        <v-avatar class="mb-4" color="grey darken-1" size="64">
+          <v-img
+            aspect-ratio="30"
+            src="https://cdn-icons-png.flaticon.com/512/6686/6686007.png"
+          />
+        </v-avatar>
+        <h2 class="black--text">Candy Land</h2>
+      </div>
+    </v-img>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-item v-for="[icon, text] in links" :key="icon" link>
+        <v-list-item-icon>
+          <v-icon>{{ icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ text }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  props: ["drawer"],
+  data() {
+    return {
+      links: [
+        ["mdi-microsoft-windows", "Dashboard"],
+        ["mdi-account", "Profile"],
+        ["mdi-clipboard-list-outline", "Products"],
+        ["mdi-card-account-details-outline", "Orders"],
+        ["mdi-cog", "System Setting"],
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,142 @@
+<template>
+  <v-app-bar app elevate-on-scroll elevation="3" color="white">
+    <v-app-bar-nav-icon @click="$emit('drawerEvent')"></v-app-bar-nav-icon>
+    <v-spacer />
+    <v-col lg="6" cols="12">
+      <v-form>
+        <v-text-field
+          class="p-0 m-0 mt-6"
+          full-width
+          dense
+          append-icon="mdi-magnify"
+          outlined
+          rounded
+          placeholder="Search"
+        />
+      </v-form>
+    </v-col>
+    <v-spacer />
+    <v-menu offset-y>
+      <template v-slot:activator="{ attrs, on }">
+        <span
+          class="mx-5 mr-10"
+          style="cursor: pointer"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-badge content="1" color="red" offset-y="10" offset-x="10">
+            <v-icon>mdi-bell</v-icon>
+          </v-badge>
+        </span>
+      </template>
+      <v-list three-line width="250">
+        <template v-for="(item, index) in items">
+          <v-subheader
+            v-if="item.header"
+            :key="item.header"
+            v-text="item.header"
+          ></v-subheader>
+
+          <v-divider
+            v-else-if="item.divider"
+            :key="index"
+            :inset="item.inset"
+          ></v-divider>
+
+          <v-list-item v-else :key="item.title">
+            <v-list-item-avatar>
+              <v-img :src="item.avatar"></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title v-html="item.title"></v-list-item-title>
+              <v-list-item-subtitle
+                v-html="item.subtitle"
+              ></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-menu>
+    <v-menu offset-y>
+      <template v-slot:activator="{ attrs, on }">
+        <span style="cursor: pointer" v-bind="attrs" v-on="on">
+          <v-chip link>
+            <v-badge dot bottom color="green" offset-y="10" offset-x="10">
+              <v-avatar size="40">
+                <v-img src="https://randomuser.me/api/portraits/women/76.jpg" />
+              </v-avatar>
+            </v-badge>
+            <span class="ml-3">Sandra Buli</span>
+          </v-chip>
+        </span>
+      </template>
+      <v-list width="250" class="py-0">
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/women/76.jpg" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Sandra Buli</v-list-item-title>
+            <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider />
+        <v-list-item link v-for="(menu, i) in menus" :key="i">
+          <v-list-item-icon>
+            <v-icon>{{ menu.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            {{ menu.title }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Topbar",
+  data() {
+    return {
+      menus: [
+        { title: "Profile", icon: "mdi-account" },
+        { title: "Change Password", icon: "mdi-key" },
+        { title: "Setting", icon: "mdi-cog" },
+        { title: "Logout", icon: "mdi-logout" },
+      ],
+      items: [
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+          title: "Brunch this weekend?",
+          subtitle: `<span class="text--primary">Joey Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+          title: "Lorem ipsum",
+          subtitle:
+            '<span class="text--primary">Jennifer Adams</span> &mdash; Do you have recommendations? Which is your best seller?',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
+          title: "Birthday gift",
+          subtitle:
+            '<span class="text--primary">Diane Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+          title: 'BBQ Recipe <span class="grey--text text--lighten-1">4</span>',
+          subtitle: `<span class="text--primary">Ross Siskoq</span> &mdash; Hope this turns out great! Check the attachment`,
+        }
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
